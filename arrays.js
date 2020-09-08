42d337f05338341476542e07bd83ff3a7ccbfabe
Michael Naquin <mnaquin1@yahoo.com>
2020-09-08 16:59:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,8 @@ return [...array, element]
 function accessElementInArray(array, index){
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+array.shift()
+  return array
+}
